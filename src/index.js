47f680ceee273d6592babeaf07b6ecd323752285
jarivuonos <50275238+jarivuonos@users.noreply.github.com>
2022-12-22 14:47:34 +0200

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./components/App";
-//alert ("index.js alkaa");
-ReactDOM.render(<App />, document.getElementById("root"));
-//ReactDOM.render(<p>hi there</p>, document.getElementById("root"));
